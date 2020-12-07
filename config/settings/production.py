af3c9a7ab0c3e50d79f4685c6029c7c5b180e49c
nhattran2311 <tranminhnhat2311@gmail.com>
2020-12-07 16:27:42 +0700

    3

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,16 +8,16 @@ ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'fantastic_db',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'Winter19!',
-        'CONN_MAX_AGE': 500
-        # 'HOST':'localhost',
-        # 'PORT':'5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         # 'NAME': 'fantastic_db',
+#         # 'USER': 'postgres',
+#         # 'PASSWORD': 'Winter19!',
+#         'CONN_MAX_AGE': 500
+#         # 'HOST':'localhost',
+#         # 'PORT':'5432',
+#     }
+# }
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
