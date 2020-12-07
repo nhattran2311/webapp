692d81fa7f44980abdc05e3fa18eeefac54406d1
nhattran2311 <tranminhnhat2311@gmail.com>
2020-12-07 16:18:24 +0700

    database

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,5 @@
 from .base import *
-
+import dj_database_url
 DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
@@ -14,7 +14,9 @@ DATABASES = {
         'NAME': 'fantastic_db',
         'USER': 'postgres',
         'PASSWORD': 'Winter19!',
+        'CONN_MAX_AGE': 500
         # 'HOST':'localhost',
         # 'PORT':'5432',
     }
 }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
