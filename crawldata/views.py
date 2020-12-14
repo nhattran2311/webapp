95a7f8d451ccd890e07e12e73dd03c11c09bf243
nhattran2311 <tranminhnhat2311@gmail.com>
2020-12-14 10:19:29 +0700

    1

--- crawldata/views.py
+++ crawldata/views.py
@@ -47,7 +47,6 @@ def create_article(request):
         r = requests.post(API_URL, auth=(API_USER, API_PASSWORD), data=data_craw)
         if r.status_code == 201:
             messages.success(request, f'Your data has been updated!')
-            data_craw = None
             return render(request, 'crawldata/craw_data_viblo.html')
         else:
             messages.error(request,f'Wrong data format - status code : {r.status_code}',)
